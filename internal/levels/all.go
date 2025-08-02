06b468dc73ce7cbe3aec0799f34ea868c662e2f8
Michael Vasquez <michael.a.vasquez.gm@gmail.com>
2025-08-02 04:27:43 -0400

    Put levels in same file

--- internal/levels/all.go
+++ internal/levels/all.go
@@ -1,5 +1,9 @@
 package levels
 
+const LEVEL_1_CHART = `------------------------
+o_o_o_o_o_o_o_o_o_o
+o o o o o o o o o o`
+
 var ALL_LEVEL_CHARTS = []string{
 	LEVEL_1_CHART,
 }
--- internal/levels/level_1.go
+++
@@ -1,5 +0,0 @@
-package levels
-
-const LEVEL_1_CHART = `------------------------
-o_o_o_o_o_o_o_o_o_o
-o o o o o o o o o o`
