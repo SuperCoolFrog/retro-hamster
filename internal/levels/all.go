5e9796baf716c178b9cc9150e72bb0e37960d2af
Michael Vasquez <michael.a.vasquez.gm@gmail.com>
2025-08-03 12:53:32 -0400

    FINAL

--- internal/levels/all.go
+++ internal/levels/all.go
@@ -5,12 +5,22 @@ _o_o_o_o_M_o_o_o
 _o_S_o_m_o_Q_o_o`
 
 const LEVEL_2_CHART = `------------------------
-_o_o_o_o_S_o_o_o_o_S
+_o_M_o_o_S_o_o_o_o_S
 _o o o o SS o S o o o`
+
+const LEVEL_3_CHART = `------------------------
+_o_o_o_o_Q_m_Q_o_S_o_
+_Q o o o QQ o S o o o`
+
+const LEVEL_4_CHART = `------------------------
+_o_o_|_o_S_m_Q_o_S_o_
+_Q o | o M o S o o o`
 
 var ALL_LEVEL_CHARTS = []string{
 	LEVEL_1_CHART,
-	// LEVEL_2_CHART,
+	LEVEL_2_CHART,
+	LEVEL_3_CHART,
+	LEVEL_4_CHART,
 }
 
 const LEVEL_BOSS_1 = `------------------------
@@ -21,7 +31,7 @@ const LEVEL_BOSS_2 = `------------------------
 _m___m_o_2_o_o_m_o`
 
 const LEVEL_BOSS_3 = `------------------------
-_______|_3|______`
+_______|3|______`
 
 var BOSS_LEVELS = []string{
 	LEVEL_BOSS_1,
